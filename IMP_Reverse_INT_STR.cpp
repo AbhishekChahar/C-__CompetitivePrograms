1953fb61458a2e1371779c7d6fde763949168612
Abhishek Chahar <37897749+AbhishekChahar@users.noreply.github.com>
2019-09-12 01:56:01 +0530

    Add files via upload

---
+++ IMP_Reverse_INT_STR.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+int main(){
+    
+    // int a ;
+    // cin>>a;                                              //Integer number
+    // int new_number=0;
+    // while(a != 0) {                                             
+    //     new_number = ( new_number*10) + a%10;
+    //     a = a/10;
+    // }
+    
+    cout<<new_number;                                    //String object
+    
+    string str;
+    cin>>str;
+    reverse(str.begin(), str.end());
+    cout<<str;
+    
+    return 0;
+}
